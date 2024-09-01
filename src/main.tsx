eca419d9fc1d94bd32c3d6ced5c5175be7350329
Manav Rathi <manav@mrmr.io>
2024-09-01 19:34:37 +0530

    Try with basename

--- src/main.tsx
+++ src/main.tsx
@@ -18,17 +18,20 @@ import { Day25 } from "./25/index.tsx";
 // Link causes problems with scroll going all over the place. To keep things
 // simple, we take the worst of both worlds. A SPA with HTML navigation.
 
-const router = createBrowserRouter([
-    { path: "/gen24/1", element: <Day1 /> },
-    { path: "/gen24/2", element: <Day2 /> },
-    { path: "/gen24/4", element: <Day4 /> },
-    { path: "/gen24/8", element: <Day8 /> },
-    { path: "/gen24/11", element: <Day11 /> },
-    { path: "/gen24/18", element: <Day18 /> },
-    { path: "/gen24/25", element: <Day25 /> },
-    { path: "/gen24/cover", element: <Cover /> },
-    { path: "/gen24/*", element: <Index /> },
-]);
+const router = createBrowserRouter(
+    [
+        { path: "/1", element: <Day1 /> },
+        { path: "/2", element: <Day2 /> },
+        { path: "/4", element: <Day4 /> },
+        { path: "/8", element: <Day8 /> },
+        { path: "/11", element: <Day11 /> },
+        { path: "/18", element: <Day18 /> },
+        { path: "/25", element: <Day25 /> },
+        { path: "/cover", element: <Cover /> },
+        { path: "/*", element: <Index /> },
+    ],
+    { basename: "/gen24" }
+);
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>

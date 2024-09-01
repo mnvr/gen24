f50cef38c7797c4e03b3f2bc160fc9bb89fbba8a
Manav Rathi <manav@mrmr.io>
2024-09-01 13:31:24 +0530

    MPA 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, "index.html"),
+                "1": resolve(__dirname, "src/1/index.html"),
+            },
+        },
+    },
 });
